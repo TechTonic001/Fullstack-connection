687b970984184385e320d5b724c4d3c470089bca
TechTonic <amoohalleluyah1@gmail.com>
2026-02-11 09:01:08 +0100

    class

--- Backend/index.js
+++ Backend/index.js
@@ -6,12 +6,20 @@ const Cors = require('cors')
 
 
 app.use(Cors())
+app.use(epxress.json())
 
 require('./connection')
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+app.post('/info',(req,res)=>{
+    const {name,email,password} = req.body;
+    let details = {name, email, password}
+    res.status(201).json({message:details})
+})
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,25 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 const App = () => {
   const endpoint = 'http://localhost:4000/'
+  const endpoint2 = 'http://localhost:4000/info/'
+
+  const [name, setname] = useState()
+  const [email, setemail] = useState()
+  const [password, setpassword] = useState()
   const MakeRequest = () => {
-    axios.get(endpoint)
+    axios.post(endpoint + 'info', { name, email, password })
       .then((result) => {
         console.log(result);
       })
       .catch((err) => {
         console.log(err);
-        
+
       })
   };
+
+  const submitDetails = () => {
+    const information = { name, email, password }
+    axios.post(endpoint2, information)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  
 
   return (
     <>
       <h1>Frontend and Backend Connected</h1>
       <button onClick={MakeRequest}>Make Request</button>
+      <hr />
+      <input onChange={(e) => { setname(e.target.value) }} type="text" placeholder='name' value={name} />
+      <input onChange={(e) => { setemail(e.target.value) }} type="text" placeholder='email' value={email} />
+      <input onChange={(e) => { setpassword(e.target.value) }} type="text" placeholder='password' value={password} />
+      <button onClick={submitDetails}>Submit</button>
     </>
-
   )
 }
 
